89dd841edc2ce2f6fd01484a5788d03c5215886f
HowLargeTheTeeth <graham.emily15@gmail.com>
2021-09-20 00:28:37 +1000

    added responsive design to navbar

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -1,4 +1,11 @@
 // changes opacity of selected nav__link
 // const navSelection = (selection) => {
-
+  // iterate through each link
+    // for each link 
+      // reduce opacity to 40%
+      // and circle size
+    // if link = event trigger (selection) 
+      // become 100% opaque
+      // and increase circle size
+      // and activate pulse animation
 // } 
