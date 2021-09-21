425a7dc583cbaff49dc593fbc000b4cd9026b791
HowLargeTheTeeth <graham.emily15@gmail.com>
2021-09-21 15:02:59 +1000

    formated loading bar flex box, added temporary bar animation and linked temp animations through js

--- javascript/loading_behaviour.js
+++ javascript/loading_behaviour.js
@@ -1,31 +1,40 @@
 // variables 
-const preloader = document.querySelector('#preloader');
 const overflow = document.querySelector('body');
+const preloader = document.querySelector('#preloader');
 const letsGo = document.querySelector("#letsGo");
+const letsGoLabel = document.querySelector(".letsGo__label");
+const loadingBar = document.querySelector(".loading-bar");
+const loadingSlider = document.querySelector(".loading-bar__progress");
 const nav = document.querySelector(".nav");
+// Variable not yet called
+const loadingSliderWidth = loadingSlider.style.width;
 
-// loading bar animation logic
+// when loadingSliderWidth === 100%, call pageLoaded()
 
-// on loading 100%, hide loading bar
-
-// once window loads, activate letsGo function
-// document.addEventListener("load", letsGo());
-
-// const letsGo = () => {
-//   // make button appear
-// }
+//(CURRENTLY NOT LINKED TO PAGE PROGRESS)
+const pageLoaded = () => {
+  // animate vanishing loading bar
+  loadingBar.classList.add("loading-bar--animated");
+  // animate appearing button/gif
+  letsGo.classList.add("letsGo--appear");
+  letsGo.classList.add("letsGo__pulse--appear");
+  // animate text fade in
+  letsGoLabel.classList.add("letsGo__label--appear");
+}
 
 // letsGoButton, onclick 
 const preloaderFade = () => {
   console.log("I'm being clicked");
   // black fades out
   preloader.classList.add("preloader--disappear");
-  // loading bar deletes
   // activate navbar animation 
   nav.classList.add("nav--animated");
   // delete preloader--overflow class from body
-    overflow.classList.remove("preloader--overflow");
+  overflow.classList.remove("preloader--overflow");
 }
+
+// once window loads, activate letsGo function
+// document.addEventListener("load", pageLoaded);
 
 // listen for the letsGo Button event
 letsGo.addEventListener("click", preloaderFade);
